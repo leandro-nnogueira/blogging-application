899302dd0e784bc4b072ed8049c7fdcd308d9b43
Leandro Nascimento Nogueira <LeandroNascimentoNogueira@gmail.com>
2025-02-07 17:16:45 -0300

    feat: Entity->Post

---
+++ blogging-api/src/main/java/com/leandro/blogging_api/entity/Post.java
@@ -0,0 +1,28 @@
+package com.leandro.blogging_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+
+}
