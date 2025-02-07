2280001d92afd2e1592ed9258a830faea4f562f5
Leandro Nascimento Nogueira <LeandroNascimentoNogueira@gmail.com>
2025-02-07 17:18:57 -0300

    feat: Repository->Post

---
+++ blogging-api/src/main/java/com/leandro/blogging_api/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.leandro.blogging_api.repository;
+
+import com.leandro.blogging_api.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
